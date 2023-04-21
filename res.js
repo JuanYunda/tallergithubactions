1167d5fc1902343f0b85236bcd0b10c225a0925e
Juan Mazuera 2043008 <81594863+JuanYunda@users.noreply.github.com>
2023-04-21 18:31:55 -0500

    add functions res (substact two numbers) and mul (multiplicate two numbers)

---
+++ res.js
@@ -0,0 +1,4 @@
+function res(a, b) {
+    return a - b;
+}
+module.exports = res;
--- sum.test.js
+++ sum.test.js
@@ -3,3 +3,15 @@ const sum = require('./sum');
 test('sums two numbers', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+const res = require('./res');
+
+test('substact two numbers', () => {
+  expect(res(1, 2)).toBe(-1);
+});
+
+const mul = require('./mul');
+
+test('multiplicate two numbers', () => {
+  expect(mul(1, 2)).toBe(2);
+});
